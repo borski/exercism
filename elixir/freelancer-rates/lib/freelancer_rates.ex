d94f20e047cf3f075fc6817ec1a492ce5717b33e
Michael Borohovski <borski@synopsys.com>
2021-09-15 14:37:12 -0700

    finished freelancer rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,21 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - (before_discount * (discount / 100))
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    base_rate = 22 * daily_rate(hourly_rate)
+    discounted_rate = apply_discount(base_rate, discount)
+
+    ceil(discounted_rate)
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    number_of_days = budget / apply_discount(hourly_rate, discount) / 8
+    Float.floor(number_of_days, 1)
   end
 end
