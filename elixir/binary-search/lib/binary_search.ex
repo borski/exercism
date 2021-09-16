dca1891ae2fe5da14011d5a963f4fa8cc3c8d78c
Michael Borohovski <borski@synopsys.com>
2021-09-16 08:50:43 -0700

    finished binary search

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -18,6 +18,17 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search({}, _), do: :not_found
+  def search(numbers, key), do: _search(numbers, key, 0, tuple_size(numbers)-1)
+
+  defp _search(_, _, low, high) when low > high, do: :not_found
+  defp _search(numbers, key, low, high) do
+    mid_idx = div(low + high, 2)
+    test = elem(numbers, mid_idx)
+    cond do
+      key === test -> {:ok, mid_idx}
+      key < test -> _search(numbers, key, low, mid_idx-1)
+      key > test -> _search(numbers, key, mid_idx+1, high)
+    end
   end
 end
