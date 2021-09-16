2cb50535af02b25e3efd5ee69f617ea5f6a956a0
Michael Borohovski <borski@synopsys.com>
2021-09-16 16:57:41 -0700

    finished date parser

--- elixir/date-parser/lib/date_parser.ex
+++ elixir/date-parser/lib/date_parser.ex
@@ -1,65 +1,71 @@
 defmodule DateParser do
   def day() do
-    # Please implement the day/0 function
+    "\\d{1,2}"
   end
 
   def month() do
-    # Please implement the month/0 function
+    "\\d{1,2}"
   end
 
   def year() do
-    # Please implement the year/0 function
+    "\\d{4}"
   end
 
   def day_names() do
-    # Please implement the day_names/0 function
+    "\\b(Mon|Tues|Wednes|Thurs|Fri|Satur|Sun)(day)\\b"
   end
 
   def month_names() do
-    # Please implement the month_names/0 function
+    "\\b(January|February|March|April|May|June|July|August|September|October|November|December)\\b"
   end
 
   def capture_day() do
-    # Please implement the capture_day/0 function
+    "(?<day>#{day()})"
   end
 
   def capture_month() do
-    # Please implement the capture_month/0 function
+    "(?<month>#{month()})"
   end
 
   def capture_year() do
-    # Please implement the capture_year/0 function
+    "(?<year>#{year()})"
   end
 
   def capture_day_name() do
-    # Please implement the capture_day_name/0 function
+    "(?<day_name>#{day_names()})"
   end
 
   def capture_month_name() do
-    # Please implement the capture_month_name/0 function
+    "(?<month_name>#{month_names()})"
   end
 
   def capture_numeric_date() do
-    # Please implement the capture_numeric_date/0 function
+    "#{capture_day()}\/#{capture_month()}\/#{capture_year()}"
   end
 
   def capture_month_name_date() do
-    # Please implement the capture_month_name_date/0 function
+    "#{capture_month_name()} #{capture_day()}, #{capture_year()}"
   end
 
   def capture_day_month_name_date() do
-    # Please implement the capture_day_month_name_date/0 function
+    "#{capture_day_name()}, #{capture_month_name_date()}"
   end
 
   def match_numeric_date() do
-    # Please implement the match_numeric_date/0 function
+    "^#{capture_numeric_date()}$"
+    |> Regex.compile
+    |> elem(1)
   end
 
   def match_month_name_date() do
-    # Please implement the match_month_name_day/0 function
+    "^#{capture_month_name_date()}$"
+    |> Regex.compile
+    |> elem(1)
   end
 
   def match_day_month_name_date() do
-    # Please implement the match_day_month_name_date/0 function
+    "^#{capture_day_month_name_date()}$"
+    |> Regex.compile
+    |> elem(1)
   end
 end
