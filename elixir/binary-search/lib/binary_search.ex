8c7e13097aba6a4fe74f8625a22385bc008e53f4
Michael Borohovski <borski@synopsys.com>
2021-09-16 09:43:17 -0700

    making binary search more elixiry

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -19,16 +19,16 @@ defmodule BinarySearch do
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search({}, _), do: :not_found
-  def search(numbers, key), do: _search(numbers, key, 0, tuple_size(numbers)-1)
+  def search(numbers, key), do: do_search(numbers, key, 0, tuple_size(numbers)-1)
 
-  defp _search(_, _, low, high) when low > high, do: :not_found
-  defp _search(numbers, key, low, high) do
+  defp do_search(_, _, low, high) when low > high, do: :not_found
+  defp do_search(numbers, key, low, high) do
     mid_idx = div(low + high, 2)
     test = elem(numbers, mid_idx)
     cond do
       key === test -> {:ok, mid_idx}
-      key < test -> _search(numbers, key, low, mid_idx-1)
-      key > test -> _search(numbers, key, mid_idx+1, high)
+      key < test -> do_search(numbers, key, low, mid_idx-1)
+      key > test -> do_search(numbers, key, mid_idx+1, high)
     end
   end
 end
