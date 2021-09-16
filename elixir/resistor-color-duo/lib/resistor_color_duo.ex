4f5550fbd71c51be7d028a502b6d4c24d034136a
Michael Borohovski <borski@synopsys.com>
2021-09-16 09:56:32 -0700

    finished resistor color duo

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -4,5 +4,19 @@ defmodule ResistorColorDuo do
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
+    color_map = %{
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9
+    }
+    
+    color_map[Enum.at(colors, 0)] * 10 + color_map[Enum.at(colors, 1)]
   end
 end
