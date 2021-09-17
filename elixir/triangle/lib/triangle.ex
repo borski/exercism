205d99b2668f8fa9aa20ab342104d793530c4d82
Michael Borohovski <borski@synopsys.com>
2021-09-17 15:47:09 -0700

    finished triangle

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -5,6 +5,12 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+
+  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0, do: {:error, "all side lengths must be positive"}
+  def kind(a, b, c) when a + b < c or a + c < b or b + c < a, do: {:error, "side lengths violate triangle inequality"}
+  def kind(a, b, c) when a === b and b === c, do: {:ok, :equilateral}
+  def kind(a, b, c) when a !== b and b !== c and a !== c, do: {:ok, :scalene}
   def kind(a, b, c) do
+    if [a,b,c] |> Enum.uniq |> length >= 2, do: {:ok, :isosceles}
   end
 end
