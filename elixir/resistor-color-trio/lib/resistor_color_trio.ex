498866486b10e93190454a74907bba98cb333ff2
Michael Borohovski <borski@synopsys.com>
2021-09-16 19:17:53 -0700

    finished resistor color trio

--- elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -4,5 +4,34 @@ defmodule ResistorColorTrio do
   """
   @spec label(colors :: [atom]) :: {integer, :ohms | :kiloohms}
   def label(colors) do
+    color_map = %{
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9
+    }
+
+    value = colors
+            |> Enum.map(&(color_map[&1]))
+            |> Enum.take(2)
+
+    multiplier = colors
+                 |> Enum.map(&(color_map[&1]))
+                 |> List.last
+                 |> (&List.duplicate(0,&1)).()
+
+    total_value = Integer.undigits(value ++ multiplier)
+
+    if total_value > 1000 do
+      {total_value / 1000, :kiloohms}
+    else
+      {total_value, :ohms}
+    end
   end
 end
