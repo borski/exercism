d8109dd93928ae7aab20b58e3022d66e3b5a7ef0
Michael Borohovski <borski@synopsys.com>
2021-09-17 15:50:08 -0700

    improved triangle

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -8,9 +8,11 @@ defmodule Triangle do
 
   def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0, do: {:error, "all side lengths must be positive"}
   def kind(a, b, c) when a + b < c or a + c < b or b + c < a, do: {:error, "side lengths violate triangle inequality"}
-  def kind(a, b, c) when a === b and b === c, do: {:ok, :equilateral}
-  def kind(a, b, c) when a !== b and b !== c and a !== c, do: {:ok, :scalene}
   def kind(a, b, c) do
-    if [a,b,c] |> Enum.uniq |> length >= 2, do: {:ok, :isosceles}
+    case [a,b,c] |> Enum.uniq |> length do
+      1 -> {:ok, :equilateral}
+      2 -> {:ok, :isosceles}
+      3 -> {:ok, :scalene}
+    end
   end
 end
