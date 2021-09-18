d923c71744089cb61ea717117e0d3917d72559af
Michael Borohovski <borski@mit.edu>
2021-09-17 19:21:36 -0700

    finished take a number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,19 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop() end)
+  end
+
+  def loop(state \\ 0) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        loop(state)
+      {:take_a_number, sender_pid} -> 
+        state = state + 1
+        send(sender_pid, state)
+        loop(state)
+      :stop -> :ok
+      _ -> loop(state)
+    end
   end
 end
