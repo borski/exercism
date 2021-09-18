2a891f99c1aa00dcc1a3d90cbeb4fa4852a3b7bd
Michael Borohovski <borski@mit.edu>
2021-09-17 21:06:22 -0700

    finish dna encoding

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,29 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0b0000
+      ?A -> 0b0001
+      ?C -> 0b0010
+      ?G -> 0b0100
+      ?T -> 0b1000
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0b0000 -> ?\s
+      0b0001 -> ?A
+      0b0010 -> ?C
+      0b0100 -> ?G
+      0b1000 -> ?T
+    end
   end
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
+  def encode([]), do: <<>>
+  def encode([c | tail]), do: <<(<<encode_nucleotide(c)::4>>)::bitstring, encode(tail)::bitstring>>
 
-  def decode(dna) do
-    # Please implement the decode/1 function
+  def decode(<<>>), do: ''
+  def decode(<<value::4, rest::bitstring>>) do
+    [decode_nucleotide(value)] ++ decode(rest)
   end
 end
