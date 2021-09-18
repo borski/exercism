24cc42f07d4e13d14e2b97a2208f9f500be4a23c
Michael Borohovski <borski@mit.edu>
2021-09-18 01:50:25 -0700

    finished gigasecond

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -5,5 +5,8 @@ defmodule Gigasecond do
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
   def from({{year, month, day}, {hours, minutes, seconds}}) do
+    NaiveDateTime.new!(year, month, day, hours, minutes, seconds)
+    |> NaiveDateTime.add(1_000_000_000)
+    |> NaiveDateTime.to_erl
   end
 end
