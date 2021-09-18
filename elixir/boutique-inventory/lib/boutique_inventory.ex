aa3b0ba9dd921f5a75dc349e4dc301ecce164894
Michael Borohovski <borski@mit.edu>
2021-09-18 14:51:57 -0700

    finish boutique inventory

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,17 +1,16 @@
 defmodule BoutiqueInventory do
-  def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
-  end
+  def sort_by_price(inventory), do: Enum.sort_by(inventory, & &1.price)
 
-  def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
-  end
+  def with_missing_price(inventory), do: Enum.filter(inventory, &is_nil(&1.price))
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    Map.replace(
+      item,
+      :quantity_by_size,
+      item.quantity_by_size |> Enum.map(fn {size, n} -> {size, n + count} end) |> Enum.into(%{})
+    )
   end
 
-  def total_quantity(item) do
-    # Please implement the total_quantity/1 function
-  end
+  def total_quantity(item), do: Enum.reduce(item.quantity_by_size, 0, &(elem(&1, 1) + &2))
+  # Another option: Enum.reduce(item.quantity_by_size, 0, fn {_, count}, acc -> count + acc end)
 end
