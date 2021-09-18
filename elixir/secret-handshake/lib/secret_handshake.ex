29f98c1241ca730cc50a820739d734859d70e197
Michael Borohovski <borski@mit.edu>
2021-09-18 15:28:28 -0700

    finish secret handshake

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -13,7 +13,23 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+  use Bitwise, only_operators: true
+
   @spec commands(code :: integer) :: list(String.t())
+
   def commands(code) do
+    []
+    |> handshake(code &&& 0b00001)
+    |> handshake(code &&& 0b00010)
+    |> handshake(code &&& 0b00100)
+    |> handshake(code &&& 0b01000)
+    |> handshake(code &&& 0b10000)
   end
+
+  defp handshake(parts, 0b00001), do: parts ++ ["wink"]
+  defp handshake(parts, 0b00010), do: parts ++ ["double blink"]
+  defp handshake(parts, 0b00100), do: parts ++ ["close your eyes"]
+  defp handshake(parts, 0b01000), do: parts ++ ["jump"]
+  defp handshake(parts, 0b10000), do: Enum.reverse(parts)
+  defp handshake(parts, _), do: parts
 end
