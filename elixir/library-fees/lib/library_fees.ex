67d4fb1d51faf4c1ac6a92268bbd524b36fb6484
Michael Borohovski <borski@mit.edu>
2021-09-18 01:45:17 -0700

    finish library fees

--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -1,25 +1,32 @@
 defmodule LibraryFees do
-  def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
-  end
+  def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
 
-  def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
-  end
+  def before_noon?(datetime), do: Time.compare(datetime, ~T[12:00:00]) === :lt
+
+  defp days_to_seconds(days), do: days * 24 * 60 * 60
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    if before_noon?(checkout_datetime) do
+      checkout_datetime |> NaiveDateTime.add(days_to_seconds(28)) |> NaiveDateTime.to_date
+    else  
+      checkout_datetime |> NaiveDateTime.add(days_to_seconds(29)) |> NaiveDateTime.to_date
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
-  def monday?(datetime) do
-    # Please implement the monday?/1 function
-  end
+  def monday?(datetime), do: Date.day_of_week(datetime) === 1
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    checkout = datetime_from_string(checkout)
+    return = datetime_from_string(return)
+
+    fees = rate * days_late(return_date(checkout), return)
+
+    if monday?(return), do: div(fees, 2), else: fees
   end
 end
