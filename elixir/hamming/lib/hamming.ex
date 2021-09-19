89127959dc83b3c2b221d1f0029ca7c480f59d24
Michael Borohovski <borski@mit.edu>
2021-09-19 14:42:41 -0700

    finish hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -8,6 +8,20 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+
+  def hamming_distance(strand1, strand2) when length(strand1) !== length(strand2),
+    do: {:error, "strands must be of equal length"}
+
   def hamming_distance(strand1, strand2) do
+    {
+      :ok,
+      Enum.reduce(
+        Enum.zip(strand1, strand2),
+        0,
+        fn {a, b}, acc ->
+          if a !== b, do: acc + 1, else: acc
+        end
+      )
+    }
   end
 end
