cdace2a592df35fe91ccf4f26d568dd453e097cd
Michael Borohovski <borski@mit.edu>
2021-09-19 14:23:32 -0700

    finish anagram

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,19 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    base = String.downcase(base)
+    base_letters = split_and_sort(base)
+
+    Enum.filter(
+      candidates,
+      &(split_and_sort(&1) === base_letters and String.downcase(&1) != base)
+    )
+  end
+
+  defp split_and_sort(word) do
+    word
+    |> String.downcase()
+    |> String.split("", trim: true)
+    |> Enum.sort()
   end
 end
