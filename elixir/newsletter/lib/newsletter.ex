5d244029f039d09bf92ccb3c9753bdc95b6a13b7
Michael Borohovski <borski@mit.edu>
2021-09-20 03:43:22 -0700

    finish newsletter

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,22 @@
 defmodule Newsletter do
-  def read_emails(path) do
-    # Please implement the read_emails/1 function
-  end
+  def read_emails(path), do: File.read!(path) |> String.split("\n", trim: true)
 
-  def open_log(path) do
-    # Please implement the open_log/1 function
-  end
+  def open_log(path), do: File.open!(path, [:write])
 
-  def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
-  end
+  def log_sent_email(pid, email), do: IO.puts(pid, email)
 
-  def close_log(pid) do
-    # Please implement the close_log/1 function
-  end
+  def close_log(pid), do: File.close(pid)
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    emails = read_emails(emails_path)
+    log = open_log(log_path)
+
+    Enum.each(emails, fn email ->
+      if send_fun.(email) === :ok do
+        log_sent_email(log, email)
+      end
+    end)
+
+    close_log(log)
   end
 end
