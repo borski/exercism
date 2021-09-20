87cba10391925d20187ae2d6b2a311fca0a295d9
Michael Borohovski <borski@mit.edu>
2021-09-20 03:59:09 -0700

    finish chessboard

--- elixir/chessboard/lib/chessboard.ex
+++ elixir/chessboard/lib/chessboard.ex
@@ -1,17 +1,9 @@
 defmodule Chessboard do
-  def rank_range do
-    # Please implement the rank_range/0 function
-  end
+  def rank_range, do: 1..8
 
-  def file_range do
-    # Please implement the file_range/0 function
-  end
+  def file_range, do: ?A..?H
 
-  def ranks do
-    # Please implement the ranks/0 function
-  end
+  def ranks, do: rank_range() |> Enum.to_list()
 
-  def files do
-    # Please implement the files/0 function
-  end
+  def files, do: file_range() |> Enum.to_list() |> Enum.map(&<<&1::utf8>>)
 end
