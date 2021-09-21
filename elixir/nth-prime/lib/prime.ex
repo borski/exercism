0c9a3ace1dffb26a11562c28cf1dd38378262b56
Michael Borohovski <borski@mit.edu>
2021-09-21 00:48:04 -0700

    finish nth prime

--- elixir/nth-prime/lib/prime.ex
+++ elixir/nth-prime/lib/prime.ex
@@ -3,6 +3,18 @@ defmodule Prime do
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
+  def nth(0), do: raise(ArgumentError)
+
   def nth(count) do
+    Stream.iterate(2, &(&1 + 1))
+    |> Stream.filter(&is_prime?/1)
+    |> Enum.at(count - 1)
+  end
+
+  @spec is_prime?(non_neg_integer) :: boolean
+  def is_prime?(2), do: true
+
+  def is_prime?(n) do
+    Enum.all?(2..ceil(:math.sqrt(n)), &(rem(n, &1) !== 0))
   end
 end
