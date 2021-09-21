264ffcf64a6fbfa513f180a1b6357a5a1de7e4a3
Michael Borohovski <borski@mit.edu>
2021-09-21 02:39:38 -0700

    finish protein translation

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,41 @@
 defmodule ProteinTranslation do
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
   def of_rna(rna) do
+    proteins =
+      Regex.scan(~r/.../, rna)
+      |> List.flatten()
+      |> Enum.map(&of_codon/1)
+      |> Enum.to_list()
+      |> Keyword.values()
+
+    unless "invalid codon" in proteins do
+      {:ok, Enum.take_while(proteins, &(&1 != "STOP"))}
+    else
+      {:error, "invalid RNA"}
+    end
   end
 
   @doc """
@@ -29,5 +61,10 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def of_codon(codon) do
+    if protein = Map.get(@codons, codon) do
+      {:ok, protein}
+    else
+      {:error, "invalid codon"}
+    end
   end
 end
