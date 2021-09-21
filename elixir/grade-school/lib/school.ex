aaf05d98ee2ad8380d6322d9ae8d6d72d60b67a2
Michael Borohovski <borski@synopsys.com>
2021-09-21 09:14:38 -0700

    finish grade school

--- elixir/grade-school/lib/school.ex
+++ elixir/grade-school/lib/school.ex
@@ -10,19 +10,25 @@ defmodule School do
   """
   @spec add(map, String.t(), integer) :: map
   def add(db, name, grade) do
+    unless name in List.flatten(Map.values(db)) do 
+      Map.update(
+        db, 
+        grade, 
+        [name], 
+        &([name | &1] |> Enum.sort)
+      )
+    end || db
   end
 
   @doc """
   Return the names of the students in a particular grade.
   """
   @spec grade(map, integer) :: [String.t()]
-  def grade(db, grade) do
-  end
+  def grade(db, grade), do: Map.get(db, grade, [])
 
   @doc """
   Sorts the school by grade and name.
   """
   @spec sort(map) :: [{integer, [String.t()]}]
-  def sort(db) do
-  end
+  def sort(db), do: Enum.sort(db)
 end
