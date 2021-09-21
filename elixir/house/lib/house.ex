6e5e01c08e436a2c9a28efcae672ba99088f21db
Michael Borohovski <borski@synopsys.com>
2021-09-21 08:29:10 -0700

    finish house

--- elixir/house/lib/house.ex
+++ elixir/house/lib/house.ex
@@ -1,8 +1,37 @@
 defmodule House do
+  @lines [
+    "the house that Jack built",
+    "the malt that lay in",
+    "the rat that ate",
+    "the cat that killed",
+    "the dog that worried",
+    "the cow with the crumpled horn that tossed",
+    "the maiden all forlorn that milked",
+    "the man all tattered and torn that kissed",
+    "the priest all shaven and shorn that married",
+    "the rooster that crowed in the morn that woke",
+    "the farmer sowing his corn that kept",
+    "the horse and the hound and the horn that belonged to"
+  ]
+
   @doc """
   Return verses of the nursery rhyme 'This is the House that Jack Built'.
   """
   @spec recite(start :: integer, stop :: integer) :: String.t()
   def recite(start, stop) do
+    Enum.map(start..stop, &line/1)
+    |> Enum.join()
+  end
+
+  def line(n) do
+    verse = 
+      @lines 
+      |> Enum.take(n) 
+      |> Enum.reverse() 
+      |> Enum.join(" ")
+    
+    """
+    This is #{verse}.
+    """
   end
 end
