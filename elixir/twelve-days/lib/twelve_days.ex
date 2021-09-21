aeaea0e2b9c3b2e2d66efe638c1bf62a7856c4e6
Michael Borohovski <borski@synopsys.com>
2021-09-21 11:19:13 -0700

    finish twelve days

--- elixir/twelve-days/lib/twelve_days.ex
+++ elixir/twelve-days/lib/twelve_days.ex
@@ -1,11 +1,46 @@
 defmodule TwelveDays do
+  @presents %{
+    1 => "a Partridge in a Pear Tree",
+    2 => "two Turtle Doves",
+    3 => "three French Hens",
+    4 => "four Calling Birds",
+    5 => "five Gold Rings",
+    6 => "six Geese-a-Laying",
+    7 => "seven Swans-a-Swimming",
+    8 => "eight Maids-a-Milking",
+    9 => "nine Ladies Dancing",
+    10 => "ten Lords-a-Leaping",
+    11 => "eleven Pipers Piping",
+    12 => "twelve Drummers Drumming"
+  }
+  
+  @days %{
+    1 => "first",
+    2 => "second",
+    3 => "third",
+    4 => "fourth",
+    5 => "fifth",
+    6 => "sixth",
+    7 => "seventh",
+    8 => "eighth",
+    9 => "ninth",
+    10 => "tenth",
+    11 => "eleventh",
+    12 => "twelfth"
+  }
+
   @doc """
   Given a `number`, return the song's verse for that specific day, including
   all gifts for previous days in the same line.
   """
   @spec verse(number :: integer) :: String.t()
   def verse(number) do
+    "On the #{@days[number]} day of Christmas my true love gave to me: #{presents_for_day(number)}."
   end
+
+  defp presents_for_day(1), do: @presents[1]
+  defp presents_for_day(2), do: "#{@presents[2]}, and #{@presents[1]}" 
+  defp presents_for_day(n), do: "#{@presents[n]}, #{presents_for_day(n - 1)}"
 
   @doc """
   Given a `starting_verse` and an `ending_verse`, return the verses for each
@@ -13,12 +48,14 @@ defmodule TwelveDays do
   """
   @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
   def verses(starting_verse, ending_verse) do
+    starting_verse..ending_verse
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n")
   end
 
   @doc """
   Sing all 12 verses, in order, one verse per line.
   """
   @spec sing() :: String.t()
-  def sing do
-  end
+  def sing, do: verses(1, 12)
 end
