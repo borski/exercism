75a1d20e5b991fc00eed55474b2ca533277769b0
Michael Borohovski <borski@synopsys.com>
2021-09-22 16:23:28 -0700

    finish isbn verifier

--- elixir/isbn-verifier/lib/isbn_verifier.ex
+++ elixir/isbn-verifier/lib/isbn_verifier.ex
@@ -13,5 +13,22 @@ defmodule IsbnVerifier do
   """
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
+    isbn = String.replace(isbn, "-", "")
+
+    if is_valid?(isbn) do
+      sum = isbn
+            |> String.split("", trim: true)
+            |> Enum.map(&string_to_int/1)
+            |> Enum.zip_reduce(10..1, 0, &(&1 * &2 + &3))
+
+      rem(sum, 11) == 0
+    else
+      false
+    end
   end
+
+  defp is_valid?(isbn), do: String.length(isbn) <= 10 and isbn =~ ~r/[0-9]{9}[0-9|X]{1}/
+
+  defp string_to_int("X"), do: 10
+  defp string_to_int(s), do: String.to_integer(s)
 end
