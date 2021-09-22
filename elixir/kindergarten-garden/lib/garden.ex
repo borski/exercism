4a34ecc16c5beaf66e29cc684154d5e09a939840
Michael Borohovski <borski@synopsys.com>
2021-09-22 13:32:41 -0700

    finish kindergarten garden

--- elixir/kindergarten-garden/lib/garden.ex
+++ elixir/kindergarten-garden/lib/garden.ex
@@ -8,7 +8,30 @@ defmodule Garden do
     that information in a map.
   """
 
+  @plants %{
+      ?G => :grass,
+      ?C => :clover,
+      ?R => :radishes,
+      ?V => :violets,
+    }
+
+  @students [:alice, :bob, :charlie, :david, :eve, :fred, :ginny, :harriet, :ileana, :joseph, :kincaid, :larry]
+
   @spec info(String.t(), list) :: map
-  def info(info_string, student_names) do
+  def info(info_string, student_names \\ @students) do
+    student_names = Enum.sort(student_names)
+    garden = Map.new(student_names, &({&1, {}}))
+
+    info_string
+    |> String.split()
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&Enum.chunk_every(&1, 2))
+    |> Enum.flat_map(&Enum.zip(student_names, &1))
+    |> Enum.group_by(fn {k, _} -> k end, fn {_, v} -> v end)
+    |> Enum.map(fn {k, v} -> {k, List.flatten(v)} end)
+    |> Enum.map(fn {k, v} -> {k, convert_to_plants(v)} end)
+    |> Enum.into(garden)
   end
+
+  defp convert_to_plants(list), do: Enum.map(list, &(@plants[&1])) |> List.to_tuple()
 end
