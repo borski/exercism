62a919f8112c106b473a5aa14f101fc22cab86c7
Michael Borohovski <borski@synopsys.com>
2021-09-23 10:40:08 -0700

    Use Enum.frequencies/1 instead of reduce/map, so much cleaner

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -9,8 +9,6 @@ defmodule WordCount do
     sentence
     |> String.downcase()
     |> String.split(~r/[^[:alnum:]]'|'[^[:alnum:]]|^'|'$|[^[:alnum:]-']/u, trim: true)
-    |> Enum.reduce(%{}, fn (word, freq_map) ->
-         Map.update(freq_map, word, 1, &(&1 + 1))
-       end)
+    |> Enum.frequencies()
   end
 end
