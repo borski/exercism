c1a7188ae0b6104ed68a243cd1bc271b98e1e5a1
Michael Borohovski <borski@mit.edu>
2021-09-23 02:18:30 -0700

    finish matching brackets

--- elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -4,5 +4,19 @@ defmodule MatchingBrackets do
   """
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
+    String.replace(str, ~r/[^\(\)\[\]\{\}]+/, "")
+    |> do_check()
+  end
+
+  def do_check(""), do: true
+
+  def do_check(str) do
+    consumed = String.replace(str, ~r/\(\)|\[\]|\{\}/, "")
+
+    if String.length(consumed) != String.length(str) do
+      do_check(consumed)
+    else
+      false
+    end
   end
 end
