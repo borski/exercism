bf7301762738f9b4844d1f8badeb0d0d016e29ce
Michael Borohovski <borski@synopsys.com>
2021-09-23 11:57:32 -0700

    finish pascals triangle

--- elixir/pascals-triangle/lib/pascals_triangle.ex
+++ elixir/pascals-triangle/lib/pascals_triangle.ex
@@ -5,5 +5,12 @@ defmodule PascalsTriangle do
   """
   @spec rows(integer) :: [[integer]]
   def rows(num) do
+    Stream.iterate([1], &[1 | make_row(&1)])
+    |> Enum.take(num)
+  end
+
+  defp make_row(row) do
+    Enum.chunk_every(row, 2, 1)
+    |> Enum.map(&Enum.sum/1)
   end
 end
