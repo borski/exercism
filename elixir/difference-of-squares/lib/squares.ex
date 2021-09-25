f1ade20b0706ebbf605f73f21eeb3bc85f3c27ef
Michael Borohovski <borski@synopsys.com>
2021-09-24 17:47:44 -0700

    finish difference of squares

--- elixir/difference-of-squares/lib/squares.ex
+++ elixir/difference-of-squares/lib/squares.ex
@@ -7,20 +7,17 @@ defmodule Squares do
   Calculate sum of squares from 1 to a given end number.
   """
   @spec sum_of_squares(pos_integer) :: pos_integer
-  def sum_of_squares(number) do
-  end
+  def sum_of_squares(number), do: div((2 * number + 1) * (number + 1) * number, 6);
 
   @doc """
   Calculate square of sum from 1 to a given end number.
   """
   @spec square_of_sum(pos_integer) :: pos_integer
-  def square_of_sum(number) do
-  end
+  def square_of_sum(number), do: Integer.pow(div(number * (number + 1), 2), 2)
 
   @doc """
   Calculate difference between sum of squares and square of sum from 1 to a given end number.
   """
   @spec difference(pos_integer) :: pos_integer
-  def difference(number) do
-  end
+  def difference(number), do: square_of_sum(number) - sum_of_squares(number)
 end
