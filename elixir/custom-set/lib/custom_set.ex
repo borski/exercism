91e54b353ef57f054b4bb51aa12925889643764b
Michael Borohovski <borski@mit.edu>
2021-09-29 23:37:41 -0700

    finish custom set

--- elixir/custom-set/lib/custom_set.ex
+++ elixir/custom-set/lib/custom_set.ex
@@ -1,43 +1,53 @@
 defmodule CustomSet do
+  defstruct map: %{}
+
   @opaque t :: %__MODULE__{map: map}
 
   @spec new(Enum.t()) :: t
-  def new(enumerable) do
-  end
+  def new(enumerable), do: Enum.reduce(enumerable, %__MODULE__{}, &add(&2, &1))
 
   @spec empty?(t) :: boolean
-  def empty?(custom_set) do
-  end
+  def empty?(%__MODULE__{map: map}), do: Enum.empty?(map)
 
   @spec contains?(t, any) :: boolean
-  def contains?(custom_set, element) do
-  end
+  def contains?(%__MODULE__{map: map}, element), do: Map.has_key?(map, element)
 
   @spec subset?(t, t) :: boolean
   def subset?(custom_set_1, custom_set_2) do
+    intersection(custom_set_1, custom_set_2)
+    |> equal?(custom_set_1)
   end
 
   @spec disjoint?(t, t) :: boolean
   def disjoint?(custom_set_1, custom_set_2) do
+    intersection(custom_set_1, custom_set_2)
+    |> empty?()
   end
 
   @spec equal?(t, t) :: boolean
-  def equal?(custom_set_1, custom_set_2) do
-  end
+  def equal?(%__MODULE__{map: map1}, %__MODULE__{map: map2}), do: map1 == map2
 
   @spec add(t, any) :: t
-  def add(custom_set, element) do
-  end
+  def add(custom_set, element), do: put_in(custom_set.map[element], nil)
 
   @spec intersection(t, t) :: t
-  def intersection(custom_set_1, custom_set_2) do
+  def intersection(%__MODULE__{map: map1}, custom_set_2) do
+    map1
+    |> Map.keys()
+    |> Enum.filter(&contains?(custom_set_2, &1))
+    |> new()
   end
 
   @spec difference(t, t) :: t
-  def difference(custom_set_1, custom_set_2) do
+  def difference(%__MODULE__{map: map1}, custom_set_2) do
+    map1
+    |> Map.keys()
+    |> Enum.reject(&contains?(custom_set_2, &1))
+    |> new()
   end
 
   @spec union(t, t) :: t
-  def union(custom_set_1, custom_set_2) do
+  def union(%__MODULE__{map: map1}, %__MODULE__{map: map2}) do
+    new(Map.keys(map1) ++ Map.keys(map2))
   end
 end
