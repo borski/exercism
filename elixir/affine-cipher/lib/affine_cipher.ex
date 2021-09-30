ae5f38f9971210a8f0e589e24e8e442d01db5246
Michael Borohovski <borski@mit.edu>
2021-09-30 01:00:07 -0700

    finish affine cipher

--- elixir/affine-cipher/lib/affine_cipher.ex
+++ elixir/affine-cipher/lib/affine_cipher.ex
@@ -9,6 +9,24 @@ defmodule AffineCipher do
   """
   @spec encode(key :: key(), message :: String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def encode(%{a: a, b: b}, message) do
+    with {:ok, _} <- mmi(a, 26) do
+      {
+        :ok,
+        message
+        |> clean()
+        |> Stream.map(fn c ->
+          if c in ?a..?z do
+            rem(a * (c - ?a) + b, 26) + ?a
+          else
+            c
+          end
+        end)
+        |> Stream.chunk_every(5)
+        |> Enum.map_join(" ", &to_string/1)
+      }
+    else
+      nil -> {:error, "a and m must be coprime."}
+    end
   end
 
   @doc """
@@ -16,5 +34,36 @@ defmodule AffineCipher do
   """
   @spec decode(key :: key(), message :: String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def decode(%{a: a, b: b}, encrypted) do
+    with {:ok, mmi} <- mmi(a, 26) do
+      {
+        :ok,
+        encrypted
+        |> clean()
+        |> Enum.map(fn c ->
+          if c in ?a..?z do
+            Integer.mod(mmi * (c - ?a - b), 26) + ?a
+          else
+            c
+          end
+        end)
+        |> to_string()
+      }
+    else
+      nil -> {:error, "a and m must be coprime."}
+    end
+  end
+
+  defp clean(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/\W/, "")
+    |> to_charlist()
+  end
+
+  defp mmi(a, m) do
+    case Enum.find(1..m, nil, &(rem(a * &1, m) == 1)) do
+      nil -> nil
+      x -> {:ok, x}
+    end
   end
 end
