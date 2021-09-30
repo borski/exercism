e9d7f232d0e05422404803d0bc038df22a95ada0
Michael Borohovski <borski@mit.edu>
2021-09-30 02:42:11 -0700

    finish markdown

--- elixir/markdown/lib/markdown.ex
+++ elixir/markdown/lib/markdown.ex
@@ -12,68 +12,24 @@ defmodule Markdown do
   """
   @spec parse(String.t()) :: String.t()
   def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+    m
+    |> String.split("\n")
+    |> Enum.map_join(&process_line/1)
+    |> format_tags()
   end
 
-  defp process(t) do
-    if (String.starts_with?(t, "#") && !String.starts_with?(t, "#######")) ||
-         String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
-  end
+  defp process_line("#" <> line), do: process_header(line, 1)
+  defp process_line("* " <> line), do: "<li>#{line}</li>"
+  defp process_line(line), do: "<p>#{line}</p>"
 
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
-    {to_string(String.length(h)), Enum.join(t, " ")}
-  end
+  defp process_header(" " <> line, 7), do: "<p>####### #{line}</p>"
+  defp process_header(" " <> line, level), do: "<h#{level}>#{line}</h#{level}>"
+  defp process_header("#" <> line, level), do: process_header(line, level + 1)
 
-  defp parse_list_md_level(l) do
-    t = String.split(String.trim_leading(l, "* "))
-    "<li>" <> join_words_with_tags(t) <> "</li>"
-  end
-
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
-  end
-
-  defp enclose_with_paragraph_tag(t) do
-    "<p>#{join_words_with_tags(t)}</p>"
-  end
-
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
-  end
-
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
-  end
-
-  defp replace_prefix_md(w) do
-    cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
-    end
-  end
-
-  defp replace_suffix_md(w) do
-    cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
-    end
-  end
-
-  defp patch(l) do
-    String.replace(l, "<li>", "<ul>" <> "<li>", global: false)
-    |> String.reverse()
-    |> String.replace(String.reverse("</li>"), String.reverse("</li></ul>"), global: false)
-    |> String.reverse()
+  defp format_tags(lines) do
+    lines
+    |> String.replace(~r/__([^_]+)__/, "<strong>\\1</strong>")
+    |> String.replace(~r/_([^_]+)_/, "<em>\\1</em>")
+    |> String.replace(~r/<li>.*<\/li>/, "<ul>\\0</ul>")
   end
 end
