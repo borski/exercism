bee3f8b2c292e05facda7624c5762a538113b1a1
Michael Borohovski <borski@mit.edu>
2021-10-02 01:05:15 -0700

    finish parallel letter frequency

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -8,5 +8,20 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    Task.async_stream(texts, &count/1, max_concurrency: workers)
+    |> Enum.reduce(
+      %{},
+      fn {:ok, map}, acc ->
+        Map.merge(acc, map, fn _, v1, v2 -> v1 + v2 end)
+      end
+    )
+  end
+
+  defp count(string) do
+    string
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.filter(&Regex.match?(~r/\p{L}/, &1))
+    |> Enum.reduce(%{}, fn letter, map -> Map.update(map, letter, 1, &(&1 + 1)) end)
   end
 end
