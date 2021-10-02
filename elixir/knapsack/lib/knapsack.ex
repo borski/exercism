8fea8fb1bb8d55e6ca638cb50af8144c99dea080
Michael Borohovski <borski@mit.edu>
2021-10-02 01:27:41 -0700

    finish knapsack

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -4,6 +4,17 @@ defmodule Knapsack do
   """
   @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
           integer
-  def maximum_value(items, maximum_weight) do
+
+  def maximum_value([], _), do: 0
+
+  def maximum_value([first | rest], maximum_weight) when first.weight > maximum_weight do
+    maximum_value(rest, maximum_weight)
+  end
+
+  def maximum_value([first | rest], maximum_weight) do
+    Enum.max([
+      first.value + maximum_value(rest, maximum_weight - first.weight),
+      maximum_value(rest, maximum_weight)
+    ])
   end
 end
