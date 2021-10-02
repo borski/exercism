7d14e00f1deb70752a0244ba51a3e40a31cb7447
Michael Borohovski <borski@mit.edu>
2021-10-01 22:13:53 -0700

    finish pythagorean triplet

--- elixir/pythagorean-triplet/lib/triplet.ex
+++ elixir/pythagorean-triplet/lib/triplet.ex
@@ -3,27 +3,30 @@ defmodule Triplet do
   Calculates sum of a given triplet of integers.
   """
   @spec sum([non_neg_integer]) :: non_neg_integer
-  def sum(triplet) do
-  end
+  defdelegate sum(triplet), to: Enum
 
   @doc """
   Calculates product of a given triplet of integers.
   """
   @spec product([non_neg_integer]) :: non_neg_integer
-  def product(triplet) do
-  end
+  defdelegate product(triplet), to: Enum
 
   @doc """
   Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
   """
   @spec pythagorean?([non_neg_integer]) :: boolean
-  def pythagorean?([a, b, c]) do
-  end
+  def pythagorean?([a, b, c]), do: a * a + b * b == c * c
 
   @doc """
   Generates a list of pythagorean triplets whose values add up to a given sum.
   """
   @spec generate(non_neg_integer) :: [list(non_neg_integer)]
   def generate(sum) do
+    for a <- 1..sum,
+        b <- (a + 1)..sum,
+        c = sum - a - b,
+        pythagorean?([a, b, c]) do
+      [a, b, c]
+    end
   end
 end
