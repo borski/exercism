8e6a38041b338d0cf0f2d7846bd96f1fbb11fcac
Michael Borohovski <michael.borohovski@gmail.com>
2022-03-24 23:06:05 -0700

    Initial commit

--- elixir/wordy/lib/wordy.ex
+++ elixir/wordy/lib/wordy.ex
@@ -4,5 +4,21 @@ defmodule Wordy do
   """
   @spec answer(String.t()) :: integer
   def answer(question) do
+    Regex.run(~r/What is (.*)\?/, question, capture: :all_but_first)
+    |> hd()
+    |> String.split()
+    |> Stream.map(&convert/1)
+    |> Enum.reject(&is_nil/1)
+    |> compute()
   end
+
+  defp convert("plus"), do: &+/2
+  defp convert("minus"), do: &-/2
+  defp convert("multiplied"), do: &*/2
+  defp convert("divided"), do: &//2
+  defp convert("by"), do: nil
+  defp convert(num), do: String.to_integer(num)
+
+  defp compute([n]), do: n
+  defp compute([a, op, b | rest]), do: compute([op.(a, b) | rest])
 end
