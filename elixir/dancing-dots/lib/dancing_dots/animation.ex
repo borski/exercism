60df8b4ad1898ed9f6598f2c0ec0ddd91f855481
Michael Borohovski <michael.borohovski@gmail.com>
2022-04-13 03:52:02 -0700

    finished dancing dots

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,41 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  use DancingDots.Animation
+
+  def handle_frame(dot, frame_number, _) when rem(frame_number, 4) == 0 do
+    %{dot | opacity: dot.opacity / 2}
+  end
+
+  def handle_frame(dot, _, _), do: dot
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  def init(opts) do
+    unless opts[:velocity] && is_number(opts[:velocity]) do
+      {:error,
+       "The :velocity option is required, and its value must be a number. Got: #{inspect(opts[:velocity])}"}
+    else
+      {:ok, opts}
+    end
+  end
+
+  def handle_frame(dot, frame_number, opts) do
+    %{dot | radius: dot.radius + (frame_number - 1) * opts[:velocity]}
+  end
 end
