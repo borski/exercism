9bc110b59c272a579e7ce6901841ef4e5df02787
Michael Borohovski <michael.borohovski@gmail.com>
2022-08-20 00:27:40 -0700

    finish log parser

--- elixir/log-parser/lib/log_parser.ex
+++ elixir/log-parser/lib/log_parser.ex
@@ -1,17 +1,16 @@
 defmodule LogParser do
-  def valid_line?(line) do
-    # Please implement the valid_line?/1 function
-  end
+  def valid_line?(line), do: line =~ ~r/^\[(DEBUG|INFO|WARNING|ERROR)\]/
 
-  def split_line(line) do
-    # Please implement the split_line/1 function
-  end
+  def split_line(line), do: Regex.split(~r/<[~*=-]*>/, line)
 
-  def remove_artifacts(line) do
-    # Please implement the remove_artifacts/1 function
-  end
+  def remove_artifacts(line), do: Regex.replace(~r/end-of-line(\d+)/i, line, "")
+
+  defp prefix_username(%{"username" => username}, line), do: "[USER] #{username} #{line}"
+  defp prefix_username(nil, line), do: line
 
   def tag_with_user_name(line) do
-    # Please implement the tag_with_user_name/1 function
+    captures = Regex.named_captures(~r/User\s+(?<username>\S+)/u, line)
+
+    prefix_username(captures, line)
   end
 end
