8195baaf851015380f9ddb215bc85820d9ccea0a
Michael Borohovski <michael.borohovski@gmail.com>
2022-08-30 01:33:56 -0700

    finish lucians luscious lasagna

---
+++ rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-308ae5810615c23e.d
@@ -0,0 +1,5 @@
+/Users/borski/src/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-308ae5810615c23e: src/lib.rs
+
+/Users/borski/src/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-308ae5810615c23e.d: src/lib.rs
+
+src/lib.rs:
