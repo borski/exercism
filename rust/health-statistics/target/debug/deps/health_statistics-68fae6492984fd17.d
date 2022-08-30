c007c065c11973cec6159e684a821d0396720f97
Michael Borohovski <michael.borohovski@gmail.com>
2022-08-30 01:56:44 -0700

    add resistor color

---
+++ rust/health-statistics/target/debug/deps/health_statistics-68fae6492984fd17.d
@@ -0,0 +1,5 @@
+/Users/borski/src/exercism/rust/health-statistics/target/debug/deps/health_statistics-68fae6492984fd17: tests/health-statistics.rs
+
+/Users/borski/src/exercism/rust/health-statistics/target/debug/deps/health_statistics-68fae6492984fd17.d: tests/health-statistics.rs
+
+tests/health-statistics.rs:
