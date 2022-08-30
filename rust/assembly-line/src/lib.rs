b4fec354be200b7dba144077aac885d63c2fc1b9
Michael Borohovski <michael.borohovski@gmail.com>
2022-08-30 01:50:39 -0700

    finish assembly line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,16 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let success_rate = match speed {
+        0..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        11..=u8::MAX => panic!("unexpected speed")
+    };
+
+    success_rate * (speed as f64) * 221.0
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) as u32) / 60
 }
